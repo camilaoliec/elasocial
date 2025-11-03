68698836dc501b43a7094f5fca2c6c66c386cbf1
Camila CROSET <potycamila@gmail.com>
2025-11-03 17:16:43 +0100

    feat: add main JSX files, i18n setup, and translation JSONs

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './scss/global.scss'
 import App from './App.jsx'
+import './i18n'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
